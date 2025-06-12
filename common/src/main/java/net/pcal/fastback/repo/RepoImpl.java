dc192a107b3db38cbc021dba7acf2144814ea6cf
ZX夏夜之风 <snwcreations@qq.com>
2025-06-12 21:16:25 +0800

    fix: make IS_NATIVE_GIT_ENABLED actually works for low-level Git operations

--- common/src/main/java/net/pcal/fastback/repo/RepoImpl.java
+++ common/src/main/java/net/pcal/fastback/repo/RepoImpl.java
@@ -42,10 +42,7 @@ import java.util.List;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static net.pcal.fastback.config.FastbackConfigKey.BROADCAST_ENABLED;
-import static net.pcal.fastback.config.FastbackConfigKey.BROADCAST_MESSAGE;
-import static net.pcal.fastback.config.FastbackConfigKey.IS_LOCK_CLEANUP_ENABLED;
-import static net.pcal.fastback.config.FastbackConfigKey.REMOTE_NAME;
+import static net.pcal.fastback.config.FastbackConfigKey.*;
 import static net.pcal.fastback.config.OtherConfigKey.REMOTE_PUSH_URL;
 import static net.pcal.fastback.logging.SystemLogger.syslog;
 import static net.pcal.fastback.logging.UserMessage.UserMessageStyle.BROADCAST;
@@ -55,6 +52,8 @@ import static net.pcal.fastback.logging.UserMessage.localized;
 import static net.pcal.fastback.logging.UserMessage.styledLocalized;
 import static net.pcal.fastback.logging.UserMessage.styledRaw;
 import static net.pcal.fastback.mod.Mod.mod;
+import static net.pcal.fastback.repo.PushUtils.jgit_lsRemote;
+import static net.pcal.fastback.repo.PushUtils.native_lsRemote;
 import static net.pcal.fastback.utils.EnvironmentUtils.isNativeOk;
 import static org.eclipse.jgit.util.FileUtils.RETRY;
 
@@ -187,9 +186,9 @@ class RepoImpl implements Repo {
 
     @Override
     public Set<SnapshotId> getLocalSnapshots() throws IOException {
-        final JGitSupplier<Collection<Ref>> refProvider = () -> {
+        final JGitSupplier<Collection<String>> refProvider = () -> {
             try {
-                return jgit.branchList().call();
+                return jgit.branchList().call().stream().map(Ref::getName).toList();
             } catch (GitAPIException e) {
                 throw new IOException(e);
             }
@@ -205,10 +204,14 @@ class RepoImpl implements Repo {
     public Set<SnapshotId> getRemoteSnapshots() throws IOException {
         final GitConfig conf = GitConfig.load(jgit);
         final String remoteName = conf.getString(REMOTE_NAME);
-        final JGitSupplier<Collection<Ref>> refProvider = () -> {
+        final JGitSupplier<Collection<String>> refProvider = () -> {
             try {
-                return jgit.lsRemote().setRemote(remoteName).setHeads(true).call();
-            } catch (GitAPIException e) {
+                if (conf.getBoolean(IS_NATIVE_GIT_ENABLED)) {
+                    return native_lsRemote(this, remoteName, true, false);
+                } else {
+                    return jgit_lsRemote(this, remoteName, true, false);
+                }
+            } catch (GitAPIException | ProcessException e) {
                 throw new IOException(e);
             }
         };
